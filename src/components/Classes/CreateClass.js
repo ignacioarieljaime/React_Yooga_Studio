1ac7f893b76d2af0540d78b8822c5b47a4769eb7
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2021-11-20 16:48:46 +0200

    Create Class Form established

--- src/components/Classes/CreateClass.js
+++ src/components/Classes/CreateClass.js
@@ -4,6 +4,56 @@ const CreateClass = (props) => {
 	return (
 		<>
 		<SinglePageHead pageInfo={{name:"Create Class", slug: "create"}} />
+		<div className="container-register">
+    <div className="title sign">Create Yoga Class</div>
+    <div className="content">
+      <form action="#">
+        <div className="user-details">
+          <div className="input-box">
+            <span className="details">Title</span>
+            <input type="text" placeholder="Enter class name" required />
+          </div>
+          <div className="input-box">
+            <span className="details">Type</span>
+			<select>
+				<option value="body balance">Body Balance</option>
+				<option value="hatha yoga">Hatha Yoga</option>
+				<option value="children yoga">Children Yoga</option>
+				<option value="yoga dance">Yoga Dance</option>
+            </select>
+          </div>
+
+          <div className="input-box">
+            <span className="details">Class Image</span>
+            <input type="text" placeholder="Enter image URL for class" required />
+          </div>
+		  <div className="input-box">
+            <span className="details">Class Capacity</span>
+            <input type="text" placeholder="Enter max class attendees" required />
+          </div>
+		  <div className="input-box">
+            <span className="details">Description</span>
+            <textarea rows="4" cols="30" placeholder="Enter class description" required />
+          </div>
+		  <div className="input-box">
+            <span className="details">Date</span>
+            <input type="date" placeholder="Enter max class attendees" required />
+          </div>
+		  <div className="input-box">
+            <span className="details">Class Start Time</span>
+            <input type="time" placeholder="Enter max class attendees" required />
+          </div>
+		  <div className="input-box">
+            <span className="details">Class End Time</span>
+            <input type="time" placeholder="Enter max class attendees" required />
+          </div>
+        </div>
+        <div className="button">
+          <input type="submit" value="Create Class" />
+        </div>
+      </form>
+    </div>
+  </div>
 		</>
 	)
 }
--- src/components/Team/TeamAll.js
+++ src/components/Team/TeamAll.js
@@ -1,4 +1,5 @@
 import SinglePageHead from "../SinglePageHead";
+import TeamSingleCard from "../Team/TeamSingleCard";
 
 const TeamAll = () => {
 	return (
@@ -11,74 +12,11 @@ const TeamAll = () => {
                     <h2>Expert Yoga Trainer</h2>
                 </div>
                 <div className="row">
-                    <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.0s">
-                        <div className="team-item">
-                            <div className="team-img">
-                                <img src="./img/team-1.jpg" alt="Image" />
-                                <div className="team-social">
-                                    <a href=""><i className="fab fa-twitter"></i></a>
-                                    <a href=""><i className="fab fa-facebook-f"></i></a>
-                                    <a href=""><i className="fab fa-linkedin-in"></i></a>
-                                    <a href=""><i className="fab fa-instagram"></i></a>
-                                </div>
-                            </div>
-                            <div className="team-text">
-                                <h2>Millie Harper</h2>
-                                <p>Yoga Teacher</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.2s">
-                        <div className="team-item">
-                            <div className="team-img">
-                                <img src="./img/team-2.jpg" alt="Image" /> 
-                                <div className="team-social">
-                                    <a href=""><i className="fab fa-twitter"></i></a>
-                                    <a href=""><i className="fab fa-facebook-f"></i></a>
-                                    <a href=""><i className="fab fa-linkedin-in"></i></a>
-                                    <a href=""><i className="fab fa-instagram"></i></a>
-                                </div>
-                            </div>
-                            <div className="team-text">
-                                <h2>Lilly Fry</h2>
-                                <p>Yoga Teacher</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.4s">
-                        <div className="team-item">
-                            <div className="team-img">
-                                <img src="./img/team-3.jpg" alt="Image" />
-                                <div className="team-social">
-                                    <a href=""><i className="fab fa-twitter"></i></a>
-                                    <a href=""><i className="fab fa-facebook-f"></i></a>
-                                    <a href=""><i className="fab fa-linkedin-in"></i></a>
-                                    <a href=""><i className="fab fa-instagram"></i></a>
-                                </div>
-                            </div>
-                            <div className="team-text">
-                                <h2>Elise Moran</h2>
-                                <p>Yoga Teacher</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.6s">
-                        <div className="team-item">
-                            <div className="team-img">
-                                <img src="./img/team-4.jpg" alt="Image" />
-                                <div className="team-social">
-                                    <a href=""><i className="fab fa-twitter"></i></a>
-                                    <a href=""><i className="fab fa-facebook-f"></i></a>
-                                    <a href=""><i className="fab fa-linkedin-in"></i></a>
-                                    <a href=""><i className="fab fa-instagram"></i></a>
-                                </div>
-                            </div>
-                            <div className="team-text">
-                                <h2>Kate Glover</h2>
-                                <p>Yoga Teacher</p>
-                            </div>
-                        </div>
-                    </div>
+				<TeamSingleCard />
+				<TeamSingleCard />
+				<TeamSingleCard />
+				<TeamSingleCard />
+				<TeamSingleCard />
                 </div>
             </div>
         </div>
