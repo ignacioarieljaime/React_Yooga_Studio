fe93a5e1bf0d7463e75ad943f1f0bff031aa450c
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2021-11-22 18:46:11 +0200

    Change latest classes to dynamic rendering

--- src/components/Classes/LatestClasses.js
+++ src/components/Classes/LatestClasses.js
@@ -1,3 +1,6 @@
+
+import SingleClassCard from './SingleClassCard';
+
 const LatestClasses = () => {
 
 	return (
@@ -10,119 +13,11 @@ const LatestClasses = () => {
                 </div>
 
                 <div className="row class-container">
-                    <div className="col-lg-4 col-md-6 col-sm-12 class-item filter-1 wow fadeInUp" data-wow-delay="0.0s">
-                        <div className="class-wrap">
-                            <div className="class-img">
-                                <img src="./img/class-1.jpg" alt="Image" />
-                            </div>
-                            <div className="class-text">
-                                <div className="class-teacher">
-                                    <img src="./img/teacher-1.png" alt="Image" />
-                                    <h3>Elise Moran</h3>
-                                    <a href="">+</a>
-                                </div>
-                                <h2>Pilates Yoga</h2>
-                                <div className="class-meta">
-                                    <p><i className="far fa-calendar-alt"></i>Sun, Tue, Thu</p>
-                                    <p><i className="far fa-clock"></i>9:00 - 10:00</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 col-sm-12 class-item filter-2 wow fadeInUp" data-wow-delay="0.2s">
-                        <div className="class-wrap">
-                            <div className="class-img">
-                                <img src="./img/class-2.jpg" alt="Image" />
-                            </div>
-                            <div className="class-text">
-                                <div className="class-teacher">
-                                    <img src="./img/teacher-2.png" alt="Image" />
-                                    <h3>Kate Glover</h3>
-                                    <a href="">+</a>
-                                </div>
-                                <h2>Iyengar Yoga</h2>
-                                <div className="class-meta">
-                                    <p><i className="far fa-calendar-alt"></i>Sun, Tue, Thu</p>
-                                    <p><i className="far fa-clock"></i>9:00 - 10:00</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 col-sm-12 class-item filter-3 wow fadeInUp" data-wow-delay="0.4s">
-                        <div className="class-wrap">
-                            <div className="class-img">
-                                <img src="./img/class-3.jpg" alt="Image" />
-                            </div>
-                            <div className="class-text">
-                                <div className="class-teacher">
-                                    <img src="./img/teacher-3.png" alt="Image" />
-                                    <h3>Elina Ekman</h3>
-                                    <a href="">+</a>
-                                </div>
-                                <h2>Ashtanga yoga</h2>
-                                <div className="class-meta">
-                                    <p><i className="far fa-calendar-alt"></i>Sun, Tue, Thu</p>
-                                    <p><i className="far fa-clock"></i>9:00 - 10:00</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 col-sm-12 class-item filter-4 wow fadeInUp" data-wow-delay="0.6s">
-                        <div className="class-wrap">
-                            <div className="class-img">
-                                <img src="./img/class-4.jpg" alt="Image" />
-                            </div>
-                            <div className="class-text">
-                                <div className="class-teacher">
-                                    <img src="./img/teacher-4.png" alt="Image" />
-                                    <h3>Lilly Fry</h3> 
-                                </div>
-                                <h2>Hatha Yoga</h2>
-                                <div className="class-meta">
-                                    <p><i className="far fa-calendar-alt"></i>Sun, Tue, Thu</p>
-                                    <p><i className="far fa-clock"></i>9:00 - 10:00</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 col-sm-12 class-item filter-1 wow fadeInUp" data-wow-delay="0.8s">
-                        <div className="class-wrap">
-                            <div className="class-img">
-                                <img src="./img/class-5.jpg" alt="Image" />
-                            </div>
-                            <div className="class-text">
-                                <div className="class-teacher">
-                                    <img src="./img/teacher-5.png" alt="Image" />
-                                    <h3>Olivia Yates</h3>
-                                    <a href="">+</a>
-                                </div>
-                                <h2>Kundalini Yoga</h2>
-                                <div className="class-meta">
-                                    <p><i className="far fa-calendar-alt"></i>Sun, Tue, Thu</p>
-                                    <p><i className="far fa-clock"></i>9:00 - 10:00</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 col-sm-12 class-item filter-2 wow fadeInUp" data-wow-delay="1s">
-                        <div className="class-wrap">
-                            <div className="class-img">
-                                <img src="./img/class-6.jpg" alt="Image" />
-                            </div>
-                            <div className="class-text">
-                                <div className="class-teacher">
-                                    <img src="./img/teacher-6.png" alt="Image" />
-                                    <h3>Millie Harper</h3>
-                                    <a href="">+</a>
-                                </div>
-                                <h2>Vinyasa yoga</h2>
-                                <div className="class-meta">
-                                    <p><i className="far fa-calendar-alt"></i>Sun, Tue, Thu</p>
-                                    <p><i className="far fa-clock"></i>9:00 - 10:00</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                   <SingleClassCard />
+				   <SingleClassCard />
+				   <SingleClassCard />
+				   <SingleClassCard />
+				   <SingleClassCard />
                 </div>
             </div>
         </div>
