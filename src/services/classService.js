451167afedbc1637a5f6a40fa96e707a8bc2daee
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2021-12-14 16:51:07 +0200

    Fix get all request

--- src/services/classService.js
+++ src/services/classService.js
@@ -5,7 +5,7 @@ const apiUrl = 'https://nexxita.ephedratk.com';
 
 export async function getAll()  {
 	try {
-		let response = await fetch(`${apiUrl}/wp-json/wp/v2/yogac_classes`);
+		let response = await fetch(`${apiUrl}/wp-json/wp/v2/yogac_classes?per_page=50`);
 		return response.json();
 	}catch(err) {
 		console.error(err)
